013f847631e476b30f2f39f538acd447c1791e7c
Joe Eltgroth <jeltgroth@pivotal.io>
2019-07-14 10:35:49 -0500

    Refactor redundant variable

--- build_invoices.py
+++ build_invoices.py
@@ -1,6 +1,8 @@
 from fpdf import FPDF
 import os
 
+X = 10  # Left side of the PDF page.
+
 
 def draw_box(pdf_obj, x, y, w, h, line_width, text_lines):
     """
@@ -62,7 +64,7 @@ def draw_table_header(pdf):
                ['Total', 'Tach'], ['Total', 'Hobbs'], ['Balance Due', 'Tach Time'],
                ['Gallons', 'of Fuel'], ['Price', 'per Gallon'], ['Fuel'], ['Misc.', 'Expenses']]
     pdf.set_font("helvetica", size=8, style='B')
-    x = 10
+    x = X
     y = 65
     cell_width = (pdf.w - 20) / 14
     row_height = 2 * (pdf.font_size + 1.5)
@@ -85,7 +87,7 @@ def draw_table_log_rows(pdf, y):
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
     for row in log_entries:
-        x = 10
+        x = X
         for col in row:
             draw_box(pdf, x, y, cell_width, row_height, .25, col)
             x += cell_width
@@ -98,11 +100,10 @@ def draw_table_monthly_due_row(pdf, y):
     pdf.set_font("helvetica", size=8)
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
-    x = 10
-    draw_box(pdf, x, y, cell_width, row_height, .25, monthly_due[0][0])
-    draw_box(pdf, x + cell_width, y, cell_width * 8, row_height, .25, ["Monthly Due"])
-    draw_box(pdf, x + cell_width * 9, y, cell_width, row_height, .25, monthly_due[0][1])
-    draw_box(pdf, x + cell_width * 10, y, cell_width * 4, row_height, .25, '')
+    draw_box(pdf, X, y, cell_width, row_height, .25, monthly_due[0][0])
+    draw_box(pdf, X + cell_width, y, cell_width * 8, row_height, .25, ["Monthly Due"])
+    draw_box(pdf, X + cell_width * 9, y, cell_width, row_height, .25, monthly_due[0][1])
+    draw_box(pdf, X + cell_width * 10, y, cell_width * 4, row_height, .25, '')
     return y + row_height
 
 
@@ -117,28 +118,26 @@ def draw_total_row(pdf, y):
     }
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
-    x = 10
     pdf.set_font("helvetica", size=8, style='B')
-    draw_box(pdf, x + cell_width * 6, y, cell_width, row_height, 0, ["Total:"])
+    draw_box(pdf, X + cell_width * 6, y, cell_width, row_height, 0, ["Total:"])
     pdf.set_font("helvetica", size=8)
-    draw_box(pdf, x + cell_width * 7, y, cell_width, row_height, 0.25, [totals["total_tach"]])
-    draw_box(pdf, x + cell_width * 8, y, cell_width, row_height, 0.25, [totals["total_hobbs"]])
-    draw_box(pdf, x + cell_width * 9, y, cell_width, row_height, 0.25, [totals["balance"]])
-    draw_box(pdf, x + cell_width * 10, y, cell_width, row_height, 0.25, [totals["gallons"]])
-    draw_box(pdf, x + cell_width * 11, y, cell_width, row_height, 0.25, [''])
-    draw_box(pdf, x + cell_width * 12, y, cell_width, row_height, 0.25, [totals["fuel"]])
-    draw_box(pdf, x + cell_width * 13, y, cell_width, row_height, 0.25, [totals["misc"]])
+    draw_box(pdf, X + cell_width * 7, y, cell_width, row_height, 0.25, [totals["total_tach"]])
+    draw_box(pdf, X + cell_width * 8, y, cell_width, row_height, 0.25, [totals["total_hobbs"]])
+    draw_box(pdf, X + cell_width * 9, y, cell_width, row_height, 0.25, [totals["balance"]])
+    draw_box(pdf, X + cell_width * 10, y, cell_width, row_height, 0.25, [totals["gallons"]])
+    draw_box(pdf, X + cell_width * 11, y, cell_width, row_height, 0.25, [''])
+    draw_box(pdf, X + cell_width * 12, y, cell_width, row_height, 0.25, [totals["fuel"]])
+    draw_box(pdf, X + cell_width * 13, y, cell_width, row_height, 0.25, [totals["misc"]])
     return y + row_height
 
 
 def draw_subtotal_row(pdf, y):
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
-    x = 10
     pdf.set_font("helvetica", size=8, style='B')
-    draw_box(pdf, x + cell_width * 12, y, cell_width, row_height, 0, ["Subtotal:"])
+    draw_box(pdf, X + cell_width * 12, y, cell_width, row_height, 0, ["Subtotal:"])
     pdf.set_font("helvetica", size=8)
-    draw_box(pdf, x + cell_width * 13, y, cell_width, row_height, .25, ["$41.42"])
+    draw_box(pdf, X + cell_width * 13, y, cell_width, row_height, .25, ["$41.42"])
     return y + row_height
 
 
@@ -146,13 +145,12 @@ def draw_previous_balance_row(pdf, y):
     pdf.set_font("helvetica", size=8)
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
-    x = 10
-    draw_box(pdf, x, y, cell_width * 4, row_height, 0, ["Past Amount Due:"])
-    draw_box(pdf, x + cell_width * 2, y, cell_width, row_height, .25, ["-$451.85"])
+    draw_box(pdf, X, y, cell_width * 4, row_height, 0, ["Past Amount Due:"])
+    draw_box(pdf, X + cell_width * 2, y, cell_width, row_height, .25, ["-$451.85"])
     pdf.set_font("helvetica", size=8, style='B')
-    draw_box(pdf, x + cell_width * 11, y, cell_width * 2, row_height, 0, ["Previous Balance:"])
+    draw_box(pdf, X + cell_width * 11, y, cell_width * 2, row_height, 0, ["Previous Balance:"])
     pdf.set_font("helvetica", size=8)
-    draw_box(pdf, x + cell_width * 13, y, cell_width, row_height, .25, ["-516.85"])
+    draw_box(pdf, X + cell_width * 13, y, cell_width, row_height, .25, ["-516.85"])
     return y + row_height
 
 
@@ -160,14 +158,13 @@ def draw_current_balance_due_row(pdf, y):
     pdf.set_font("helvetica", size=8)
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
-    x = 10
-    draw_box(pdf, x, y, cell_width * 4, row_height, 0, ["Amount Paid:"])
-    draw_box(pdf, x + cell_width * 2, y, cell_width, row_height, .25, ["$65.00"])
+    draw_box(pdf, X, y, cell_width * 4, row_height, 0, ["Amount Paid:"])
+    draw_box(pdf, X + cell_width * 2, y, cell_width, row_height, .25, ["$65.00"])
     pdf.set_font("helvetica", size=8, style='B')
-    draw_box(pdf, x + cell_width * 7, y, cell_width * 2, row_height, 0, ["Make Checks To:"])
-    draw_box(pdf, x + cell_width * 11, y, cell_width * 2, row_height, 0, ["Current Balance Due:"])
+    draw_box(pdf, X + cell_width * 7, y, cell_width * 2, row_height, 0, ["Make Checks To:"])
+    draw_box(pdf, X + cell_width * 11, y, cell_width * 2, row_height, 0, ["Current Balance Due:"])
     pdf.set_font("helvetica", size=8)
-    draw_box(pdf, x + cell_width * 13, y, cell_width, row_height, .25, ["-475.43"])
+    draw_box(pdf, X + cell_width * 13, y, cell_width, row_height, .25, ["-475.43"])
     return y + row_height
 
 
@@ -175,10 +172,9 @@ def draw_make_checks_to_row(pdf, y):
     pdf.set_font("helvetica", size=8)
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
-    x = 10
-    draw_box(pdf, x, y, cell_width * 4, row_height, 0, ["Previous Balance:"])
-    draw_box(pdf, x + cell_width * 2, y, cell_width, row_height, .25, ["-516.85"])
-    draw_box(pdf, x + cell_width * 7, y, cell_width * 3, row_height, 0, ["Litchfield Flying Club, Inc."])
+    draw_box(pdf, X, y, cell_width * 4, row_height, 0, ["Previous Balance:"])
+    draw_box(pdf, X + cell_width * 2, y, cell_width, row_height, .25, ["-516.85"])
+    draw_box(pdf, X + cell_width * 7, y, cell_width * 3, row_height, 0, ["Litchfield Flying Club, Inc."])
     return y + row_height
 
 
@@ -186,61 +182,56 @@ def draw_prev_bal_subtotal_row(pdf, y):
     pdf.set_font("helvetica", size=8)
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
-    x = 10
-    draw_box(pdf, x, y, cell_width * 4, row_height, 0, ["Previous Bal Subtotal:"])
-    draw_box(pdf, x + cell_width * 2, y, cell_width, row_height, .25, ["-516.85"])
+    draw_box(pdf, X, y, cell_width * 4, row_height, 0, ["Previous Bal Subtotal:"])
+    draw_box(pdf, X + cell_width * 2, y, cell_width, row_height, .25, ["-516.85"])
     return y + row_height
 
 
 def draw_payment_received_row(pdf, y):
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
-    x = 10
     pdf.set_font("helvetica", size=8, style='B')
-    draw_box(pdf, x + cell_width * 7, y, cell_width * 3, row_height, 0, ["Send Payment To:"])
-    draw_box(pdf, x + cell_width * 10, y, cell_width * 2, row_height, 0, ["Payment Received:"])
+    draw_box(pdf, X + cell_width * 7, y, cell_width * 3, row_height, 0, ["Send Payment To:"])
+    draw_box(pdf, X + cell_width * 10, y, cell_width * 2, row_height, 0, ["Payment Received:"])
     pdf.set_font("helvetica", size=8)
-    draw_box(pdf, x + cell_width * 13, y, cell_width, row_height, .25, ["0.00"])
+    draw_box(pdf, X + cell_width * 13, y, cell_width, row_height, .25, ["0.00"])
     return y + row_height
 
 
 def draw_please_include_row(pdf, y):
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
-    x = 10
     pdf.set_font("helvetica", size=8, style='B')
-    draw_box(pdf, x, y, cell_width * 3, row_height, 0, ["Please Include Statement"])
+    draw_box(pdf, X, y, cell_width * 3, row_height, 0, ["Please Include Statement"])
     pdf.set_font("helvetica", size=8)
-    draw_box(pdf, x + cell_width * 7, y, cell_width * 2, row_height, 0, ["Litchfield Flying Club"])
+    draw_box(pdf, X + cell_width * 7, y, cell_width * 2, row_height, 0, ["Litchfield Flying Club"])
     pdf.set_font("helvetica", size=8, style='B')
-    draw_box(pdf, x + cell_width * 10, y, cell_width * 2, row_height, 0, ["Check Number:"])
+    draw_box(pdf, X + cell_width * 10, y, cell_width * 2, row_height, 0, ["Check Number:"])
     pdf.set_font("helvetica", size=8)
-    draw_box(pdf, x + cell_width * 12, y, cell_width, row_height, 0, ["12345"])
+    draw_box(pdf, X + cell_width * 12, y, cell_width, row_height, 0, ["12345"])
     return y + row_height
 
 
 def draw_balance_forward_row(pdf, y):
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
-    x = 10
     pdf.set_font("helvetica", size=8, style='B')
-    draw_box(pdf, x, y, cell_width * 3, row_height, 0, ["Number On Your Check"])
+    draw_box(pdf, X, y, cell_width * 3, row_height, 0, ["Number On Your Check"])
     pdf.set_font("helvetica", size=8)
-    draw_box(pdf, x + cell_width * 7, y, cell_width * 2, row_height, 0, ["PO Box 112"])
+    draw_box(pdf, X + cell_width * 7, y, cell_width * 2, row_height, 0, ["PO Box 112"])
     pdf.set_font("helvetica", size=8, style='B')
-    draw_box(pdf, x + cell_width * 10, y, cell_width * 2, row_height, 0, ["Balance Forward:"])
+    draw_box(pdf, X + cell_width * 10, y, cell_width * 2, row_height, 0, ["Balance Forward:"])
     pdf.set_font("helvetica", size=8)
-    draw_box(pdf, x + cell_width * 13, y, cell_width, row_height, .25, ["-$475.43"])
+    draw_box(pdf, X + cell_width * 13, y, cell_width, row_height, .25, ["-$475.43"])
     return y + row_height
 
 
 def draw_last_line(pdf, y):
     cell_width = (pdf.w - 20) / 14
     row_height = pdf.font_size + 1.5
-    x = 10
     pdf.set_font("helvetica", size=8)
-    draw_box(pdf, x, y, cell_width * 3, row_height, 0, ["Terms: Full Payment by End of Billing Month"])
-    draw_box(pdf, x + cell_width * 7, y, cell_width * 2, row_height, 0, ["Litchfield, MN 55355"])
+    draw_box(pdf, X, y, cell_width * 3, row_height, 0, ["Terms: Full Payment by End of Billing Month"])
+    draw_box(pdf, X + cell_width * 7, y, cell_width * 2, row_height, 0, ["Litchfield, MN 55355"])
     return y + row_height
 
 
